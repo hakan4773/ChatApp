199cf002d2f3bcbb388e847f98cd16f5c22d6a18
hakan4773 <hb480312@gmail.com>
2025-06-06 22:06:10 +0300

    Refactor dashboard and main components by removing unused code and implementing Supabase authentication middleware. Update package dependencies for improved functionality and performance.

---
+++ chatapp/middleware.ts
@@ -0,0 +1,24 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs';
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+  const res = NextResponse.next();
+    const supabase = createMiddlewareClient({ req: request, res });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+   if (!session && request.nextUrl.pathname !== "/login") {
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = "/login";
+    return NextResponse.redirect(redirectUrl);
+  }
+
+  return res;
+}
+
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/profile/:path*", "/chat/:path*"],
+};
