164de6d726fe8bacb3add87780d4fbc1679a0dac
hakan4773 <hb480312@gmail.com>
2025-08-14 16:18:34 +0300

    Yeni sohbet kişisi ekleme işlemleri eklendi ve addNewUsers sayfası oluşturulldu.

---
+++ chatapp/app/components/AddNewUsers.tsx
@@ -0,0 +1,104 @@
+"use client"
+import { XMarkIcon } from '@heroicons/react/24/outline';
+import React, { useState } from 'react'
+import { toast } from 'react-toastify';
+import { supabase } from '../lib/supabaseClient';
+import { useUser } from '../context/UserContext';
+interface AddNewUsersProps {
+    setOpenUsers: (open: boolean) => void;
+}
+function AddNewUsers({ setOpenUsers }: AddNewUsersProps) {
+    const { user } = useUser();
+    const [newUser, setNewUser] = useState({ name: '', email: ''});
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setNewUser((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleAddUser = async () => {
+  const { name, email } = newUser;
+  const userId = user?.id; 
+
+  if (!name || !email) {
+    toast.error("Lütfen tüm alanları doldurun.");
+    return;
+  }
+
+  const { data: existingUser, error: userError } = await supabase
+    .from("users")
+    .select("*")
+    .eq("email", email)
+    .single();
+
+  if (userError || !existingUser) {
+    toast.error("Kullanıcı sistemde bulunamadı.");
+    return;
+  }
+
+  const { data: existingContact } = await supabase
+    .from("contacts")
+    .select("*")
+    .eq("owner_id", userId)
+    .eq("contact_id", existingUser.id)
+    .single();
+
+  if (existingContact) {
+    toast.error("Bu kullanıcı zaten eklenmiş.");
+    return;
+  }
+
+  const { data, error } = await supabase.from("contacts").insert({
+    owner_id: userId,
+    contact_id: existingUser.id,
+    email: email,
+    nickname: name 
+  });
+
+  if (error) {
+    toast.error("Kullanıcı eklenirken hata oluştu.");
+  } else {
+    toast.success("Yeni Kullanıcı Eklendi");
+    setNewUser({ name: '', email: ''});
+  }
+};
+
+
+  return (
+      <div className="fixed inset-0  bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-50 shadow-2xl">
+
+      <div className="bg-white relative dark:bg-gray-800 rounded-xl shadow-lg p-4 md:p-6 w-full max-w-md">
+                <button
+           onClick={() => setOpenUsers(false)}
+          className="absolute top-1 right-1 text-gray-400  hover:text-red-500 transition-colors"
+        >
+          <XMarkIcon className="h-6 w-6" />
+        </button> 
+        
+        <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200 mb-4">
+          Yeni Kullanıcı Ekle
+        </h3> 
+          <div className="border-b border-gray-200 mb-4"></div>
+        <input
+        type="text"
+        name="name"
+        className="border border-gray-300 rounded-md p-2 mb-4 w-full"
+        placeholder="İsim"
+        value={newUser.name}
+        onChange={handleInputChange}
+      />
+      <input
+        type="email"
+        name="email"
+        className="border border-gray-300 rounded-md p-2 mb-4 w-full"
+        placeholder="Email"
+        value={newUser.email}
+        onChange={handleInputChange}
+      />
+      <button className='bg-indigo-600 text-white rounded-md p-2 hover:bg-indigo-700 transition-colors' onClick={handleAddUser}>Kullanıcı Ekle</button>
+    </div>
+    </div>
+  )
+}
+
+export default AddNewUsers
--- chatapp/app/components/Users.tsx
+++ chatapp/app/components/Users.tsx
@@ -1,9 +1,11 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
 import { supabase } from "../lib/supabaseClient";
 import AddChatName from "./AddChatName";
+import AddNewUsers from "./AddNewUsers";
+import { useUser } from "../context/UserContext";
 
 interface OpenProps {
   setOpenUsers: (open: boolean) => void;
@@ -16,18 +18,35 @@ interface UserProps {
   name: string;
   email: string;
   avatar_url: string;
+  nickname: string;
 }
 
 const Users = ({ setOpenUsers,onCreateChat,name,setName }: OpenProps) => {
+  const {user}=useUser();
   const [users, setUsers] = useState<UserProps[]>([]);
   const [selectedUsers, setSelectedUsers] = useState<string[]>([]);
   const [openNameState, setOpenNameState] = useState<boolean>(false);
+  const [openNewUsers, setOpenNewUsersState] = useState<boolean>(false);
   //Kullanıcıları getir
   useEffect(() => {
     const getUsers = async () => {
       try {
-        const { data, error } = await supabase.from("users").select("*");
-        if (error) {
+         const { data, error } = await supabase
+        .from("contacts")
+         .select(`
+    contact_id,
+    nickname,
+    email,
+    users!contacts_contact_id_fkey1 (
+      id,
+      name,
+      email,
+      avatar_url
+    )
+  `).eq("owner_id", user?.id);       
+  
+       
+  if (error) {
           console.error("Kullanıcı bulunamadı", error);
           return;
         } else {
@@ -38,7 +57,7 @@ const Users = ({ setOpenUsers,onCreateChat,name,setName }: OpenProps) => {
       }
     };
     getUsers();
-  }, []);
+  }, [user]);
 
   //Çoklu kullanıcı seçimi
   const handleUserSelect = (userId: string) => {
@@ -70,6 +89,10 @@ const Users = ({ setOpenUsers,onCreateChat,name,setName }: OpenProps) => {
       console.log("Lütfen en az bir kullanıcı seçin.");
     }
   };
+
+  const handleNewUser = () => {
+    setOpenNewUsersState(true);
+  };
   return (
     <div
       className="fixed inset-0  bg-opacity-30 backdrop-blur-sm flex justify-center items-center z-50"
@@ -84,17 +107,26 @@ const Users = ({ setOpenUsers,onCreateChat,name,setName }: OpenProps) => {
           <XMarkIcon className="h-6 w-6" />
         </button>
 
-        {/* Başlık ve Ayırıcı */}
+        {/* Başlık ve Yeni kullanıcı ekleme  */}
+        <div className="flex justify-between ">
         <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-200 mb-4">
           Kullanıcı Seç
         </h3>
-        <div className="border-b border-gray-200 mb-4"></div>
+        <button onClick={handleNewUser} className="mr-6 bg-indigo-600 mb-4 text-white hover:bg-indigo-700 transition-colors rounded-md shadow-sm p-2">
+          Yeni Kullanıcı Ekle
+        </button>
+         {openNewUsers && (
+            <AddNewUsers setOpenUsers={setOpenNewUsersState}  />
+          )} 
+      </div>
+                
+       <div className="border-b border-gray-200 mb-4"></div>
 
         {/* Kullanıcı Listesi */}
         <ul className="space-y-3 max-h-[70vh] overflow-y-auto">
           {users.map((user) => (
             <li
-              key={user.id}
+              key={user?.id}
               onClick={() => handleUserSelect(user.id)}
               className={`p-4 bg-gray-50 dark:bg-gray-800 rounded-lg hover:bg-indigo-50 dark:hover:bg-gray-700 transition cursor-pointer flex items-center justify-between ${
                 selectedUsers.includes(user.id)
@@ -107,7 +139,7 @@ const Users = ({ setOpenUsers,onCreateChat,name,setName }: OpenProps) => {
                 <div className="w-10 h-10 rounded-full bg-indigo-100 flex items-center justify-center text-indigo-600 overflow-hidden">
                   <Image
                     src={user.avatar_url ? user.avatar_url : `/5.jpg`}
-                    alt={user.name}
+                    alt={user.nickname}
                     width={40}
                     height={40}
                     className="object-cover"
@@ -117,7 +149,7 @@ const Users = ({ setOpenUsers,onCreateChat,name,setName }: OpenProps) => {
                   />
                 </div>
                 <div>
-                  <p className="text-gray-800 font-medium dark:text-gray-200">{user.name}</p>
+                  <p className="text-gray-800 font-medium dark:text-gray-200">{user.nickname}</p>
                   <p className="text-sm text-gray-500 dark:text-gray-400">{user.email}</p>
                 </div>
               </div>
@@ -132,6 +164,7 @@ const Users = ({ setOpenUsers,onCreateChat,name,setName }: OpenProps) => {
               </span>{" "}
             </li>
           ))}
+          
         </ul>
         <div className="mt-4">
           <button
